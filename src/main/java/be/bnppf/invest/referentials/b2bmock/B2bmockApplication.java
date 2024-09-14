562bdb960346730dbf2876851755d5cbf75f256b
Jan Eckert <Jan.Eckert@rwth-aachen.de>
2024-09-14 09:20:01 +0200

    initial setup

---
+++ src/main/java/be/bnppf/invest/referentials/b2bmock/B2bmockApplication.java
@@ -0,0 +1,13 @@
+package be.bnppf.invest.referentials.b2bmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bmockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bmockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/bnppf/invest/referentials/b2bmock/B2bmockApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bnppf.invest.referentials.b2bmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bmockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
